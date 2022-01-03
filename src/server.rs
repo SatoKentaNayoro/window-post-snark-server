4711e86dc16e8f5e148aed4f2f3f7927ed36d487
hxuchen <apehuang123@gmail.com>
2022-01-03 13:29:36 +0800

    code logic of lock_server_if_free

--- src/server.rs
+++ src/server.rs
@@ -3,10 +3,11 @@ use crate::snark_proof_grpc::{
     BaseResponse, GetTaskResultRequest, GetTaskResultResponse, GetWorkerStatusRequest,
     SnarkTaskRequestParams, UnlockServerRequest,
 };
-use crate::status::ServerStatus;
+use crate::status::{ServerStatus, TaskStatus};
 use crate::tasks;
+use crate::tasks::TaskInfo;
 use std::sync::{Arc, Mutex};
-use std::time::Instant;
+use std::time::{Duration, Instant};
 use tokio::sync::mpsc::UnboundedSender;
 use tonic::{Request, Response, Status};
 
@@ -43,6 +44,9 @@ impl WindowPostServer {
     }
 }
 
+const SERVER_LOCK_TIME_OUT: Duration = Duration::from_secs(10);
+const SERVER_TASK_GET_BACK_TIME_OUT: Duration = Duration::from_secs(60);
+
 #[tonic::async_trait]
 impl SnarkTaskService for WindowPostServer {
     async fn do_snark_task(
@@ -56,7 +60,64 @@ impl SnarkTaskService for WindowPostServer {
         &self,
         request: Request<GetWorkerStatusRequest>,
     ) -> Result<Response<BaseResponse>, Status> {
-        todo!()
+        let mut si = match self.server_info.lock() {
+            Ok(s) => s,
+            Err(e) => return Err(Status::aborted(e.to_string())),
+        };
+        let ref req = request.into_inner();
+        match si.status {
+            ServerStatus::Free => {
+                si.task_info = TaskInfo::default();
+                // server will be locked by client with task_id here at first
+                si.status = ServerStatus::Locked;
+                si.task_info.task_id = req.task_id.clone();
+                si.last_update_time = Instant::now();
+                Ok(Response::new(BaseResponse {
+                    msg: ServerStatus::Free.to_string(),
+                }))
+            }
+            ServerStatus::Locked => {
+                // if locked too long and still not received task from miner, unlock it
+                if Instant::now().duration_since(si.last_update_time) > SERVER_LOCK_TIME_OUT {
+                    si.task_info = TaskInfo::default();
+                    si.status = ServerStatus::Locked;
+                    si.task_info.task_id = req.task_id.clone();
+                    si.last_update_time = Instant::now();
+                    Ok(Response::new(BaseResponse {
+                        msg: ServerStatus::Free.to_string(),
+                    }))
+                } else {
+                    Ok(Response::new(BaseResponse {
+                        msg: ServerStatus::Locked.to_string(),
+                    }))
+                }
+            }
+            ServerStatus::Working => {
+                // if miner do not get result back in 10min after task done or failed, drop task
+                if (si.task_info.task_status == TaskStatus::Done
+                    && Instant::now().duration_since(si.last_update_time)
+                        >= SERVER_TASK_GET_BACK_TIME_OUT)
+                    || (si.task_info.task_status == TaskStatus::Failed
+                        && Instant::now().duration_since(si.last_update_time)
+                            >= SERVER_TASK_GET_BACK_TIME_OUT)
+                {
+                    si.task_info = TaskInfo::default();
+                    si.status = ServerStatus::Locked;
+                    si.task_info.task_id = req.task_id.clone();
+                    si.last_update_time = Instant::now();
+                    Ok(Response::new(BaseResponse {
+                        msg: ServerStatus::Free.to_string(),
+                    }))
+                } else {
+                    Ok(Response::new(BaseResponse {
+                        msg: ServerStatus::Working.to_string(),
+                    }))
+                }
+            }
+            ServerStatus::Unknown => Ok(Response::new(BaseResponse {
+                msg: ServerStatus::Unknown.to_string(),
+            })),
+        }
     }
 
     async fn get_snark_task_result(
